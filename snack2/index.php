74c6caf0fe7f2bc12d06dd86202bd97af7e1fb35
wass01 <maatallawassim01@gmail.com>
2021-04-29 15:27:18 +0200

    snack 2 terminato

--- snack2/index.php
+++ snack2/index.php
@@ -2,3 +2,19 @@
 che non conosciamo nella documentazione) che name sia più lungo di 3 caratteri,
 che mail contenga un punto e una chiocciola e che age sia un numero.
 Se tutto è ok stampare “Accesso riuscito”, altrimenti “Accesso negato” -->
+<?php
+  $email = $_GET['mail'];
+  $nome = $_GET['name'];
+  $eta = $_GET['age'];
+
+  if(filter_var($email, FILTER_VALIDATE_EMAIL) &&
+  filter_var($eta, FILTER_VALIDATE_INT) && (strlen($nome) > 3)){
+    echo("Accesso riuscito");
+  } else {
+    echo("Accesso negato");
+  }
+?>
+
+<p>email: <?php echo $email ?></p>
+<p>nome: <?php echo $nome ?></p>
+<p>età: <?php echo $eta ?></p>
