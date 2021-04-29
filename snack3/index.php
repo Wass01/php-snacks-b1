9b3946aba5c9c0c1e740f4021168578ef95d3e8f
wass01 <maatallawassim01@gmail.com>
2021-04-29 17:06:05 +0200

    snack3 completato

--- snack3/index.php
+++ snack3/index.php
@@ -4,41 +4,48 @@ Ogni array figlio avrà come chiave una data in questo formato: DD/MM/YYYY
 Stampare ogni data con i relativi post. -->
 
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
   $date = [
     '12/02/2021' => [
-      'post' => 'Buongiorno',
-      'autore' => 'Paperino',
-      'testo' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit'
+      [
+        'post' => 'Buongiorno',
+        'autore' => 'Paperino',
+        'testo' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit'
+      ],
+      [
+        'post' => 'buonasera',
+        'autore' => 'pinco',
+        'testo' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit'
+      ]
     ],
     '06/03/2021' => [
-      'post' => 'Bella giornata',
-      'autore' => 'Pluto',
-      'testo' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit'
+      [
+        'post' => 'Bella giornata',
+        'autore' => 'Pluto',
+        'testo' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit'
+      ]
     ],
     '18/03/2021' => [
-      'post' => 'Wow',
-      'autore' => 'Topolino',
-      'testo' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit'
+      [
+        'post' => 'Wow',
+        'autore' => 'Topolino',
+        'testo' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit'
+      ]
     ]
   ];
-  var_dump($date);
+  // var_dump($date);
+  $listDate = ['12/02/2021', '06/03/2021', '18/03/2021'];
 
-
-  for ($i=0; $i < count($date); $i++) { ?>
-    <div>
-      <?= $partite['12/02/2021'][$i]['post'] ?>
-      <?= $partite['12/02/2021'][$i]['autore'] ?>
-      <?= $partite['12/02/2021'][$i]['testo'] ?>
-    </div>
-    <div>
-      <?= $partite['06/03/2021'][$i]['post'] ?>
-      <?= $partite['06/03/2021'][$i]['autore'] ?>
-      <?= $partite['06/03/2021'][$i]['testo'] ?>
-    </div>
-    <div>
-      <?= $partite['18/03/2021'][$i]['post'] ?>
-      <?= $partite['18/03/2021'][$i]['autore'] ?>
-      <?= $partite['18/03/2021'][$i]['testo'] ?>
-    </div>
-  <?php }
+  for ($j=0; $j < count($listDate) ; $j++) {
+    for ($i=0; $i < count($date[$listDate[$j]]); $i++) { ?>
+      <p>
+        <?= $date[$listDate[$j]][$i]['post'] ?>
+        <?= $date[$listDate[$j]][$i]['autore'] ?>
+        <?= $date[$listDate[$j]][$i]['testo'] ?>
+      </p>
+<?php }
+  }
   ?>
