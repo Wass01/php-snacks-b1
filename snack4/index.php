986e5b2f470918cdcc6079a823866c4d8c63a57d
wass01 <maatallawassim01@gmail.com>
2021-04-29 17:24:35 +0200

    Snack 4 finito

--- snack4/index.php
+++ snack4/index.php
@@ -1,3 +1,33 @@
 <!-- Creare un array contenente qualche alunno di un’ipotetica classe. Ogni alunno
 avrà Nome, Cognome e un array contenente i suoi voti scolastici.
 Stampare Nome, Cognome e la media dei voti di ogni alunno. -->
+
+<?php
+  $alunni = [
+    [
+      'nome' => 'Pinco',
+      'cognome' => 'Pallino',
+      'voti' => [7, 4, 5, 8, 6, 6, 9, 4],
+    ],
+    [
+      'nome' => 'Pippo',
+      'cognome' => 'Baudo',
+      'voti' => [5, 4, 3, 8, 8, 6, 6],
+    ],
+    [
+      'nome' => 'Ciro',
+      'cognome' => 'Ciruzzo',
+      'voti' => [6, 10, 2, 7, 5, 9],
+    ]
+  ];
+
+  var_dump($alunni);
+
+  for ($i=0; $i < count($alunni) ; $i++) { ?>
+      <div>
+        <?= $alunni[$i]['nome']?>
+        <?= $alunni[$i]['cognome']?>
+        <?= 'Media voti:' . $mediaVoti = array_sum($alunni[$i]['voti'])/count($alunni[$i]['voti']) ?>
+      </div>
+  <?php }
+?>
